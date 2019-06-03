66613516ba5a89ab805a393f0d126d4f9aa79d32
Charles Dorian Ngankam Djettchou <djettchoucharles@gmail.com>
2019-06-03 16:03:43 +0200

    metodo di modifica ed inserimento aggiunto

--- src/main/java/dd/RistoranteCtrl.java
+++ src/main/java/dd/RistoranteCtrl.java
@@ -4,14 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class RistoranteCtrl {
+public class RistoranteCtrl { 
 	@Autowired
 	RistoranteRepo repo;
 
-	@GetMapping("/restaurants")
+	@PostMapping("/restaurants")
 	public String ristorante(Model model) {
+		model.addAttribute("restaurants", repo.findAll());
+		return "/restaurants";
+	}
+	
+	//metodo seguente ci serve a fare l'inserimento e anche sempre salvare,mi ritorna il nome del file in templates
+	@PostMapping("/insRes/Save")
+	public String inserisci(@RequestParam int id,@RequestParam String name,@RequestParam String address,
+			@RequestParam int place,@RequestParam String typeOfCucina,  
+			@RequestParam int feedback,@RequestParam String priceRange,Model model) {
+		Ristorante insRes=new Ristorante(id,name,address,place,typeOfCucina,feedback,priceRange);
+		repo.save(insRes);
+		model.addAttribute("restaurants", repo.findAll());
+		return "/restaurants";
+	}
+	
+	@PostMapping("/modRes")
+	public String modifica(@RequestParam int id,@RequestParam String name,@RequestParam String address,
+			@RequestParam int place,@RequestParam String typeOfCucina,  
+			@RequestParam int feedback,@RequestParam String priceRange,Model model) {
+		Ristorante insRes=new Ristorante(id,name,address,place,typeOfCucina,feedback,priceRange);
+		repo.save(insRes);
 		model.addAttribute("restaurants", repo.findAll());
 		return "/restaurants";
 	}
