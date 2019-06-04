f5cce45b732095157c5a5663066d87e7cd4e8d02
Giuseppe <giuseppe.adamo90@gmail.com>
2019-06-04 10:48:30 +0200

    Creazione tabella red_users

--- Script/Setup.sql
+++ Script/Setup.sql
@@ -1,5 +1,7 @@
 DROP TABLE RED_RESTAURANTS;
 
+DROP SEQUENCE RESTAURANTS_SEQ;
+
 CREATE SEQUENCE RESTAURANTS_SEQ;
 
 CREATE TABLE RED_RESTAURANTS 
@@ -8,20 +10,47 @@ RESTAURANT_NAME VARCHAR2(30),
 RESTAURANT_ADDRESS VARCHAR2(30),
 SEATS INTEGER,
 TYPE_KITCHEN VARCHAR2 (30),
-FEEDBACK INTEGER,
+RESTAURANT_FEEDBACK VARCHAR2(5),
 PRICE_RANGE VARCHAR2(4));
 
 INSERT INTO RED_RESTAURANTS
-VALUES (001, 'Chinese home', 'Via Toscana 5', 30, 'Chinese', 1, '$$');
+VALUES (001, 'Chinese home', 'Via Toscana 5', 30, 'Chinese', '*', '$$');
 
 INSERT INTO RED_RESTAURANTS
-VALUES (002, 'Sicily ndo cori', 'Via Sicilia 18', 50, 'Italian', 5, '$$$');
+VALUES (002, 'Sicily ndo cori', 'Via Sicilia 18', 50, 'Italian', '*****', '$$$');
 
 INSERT INTO RED_RESTAURANTS
-VALUES (003, 'Green Irish', 'Via Dublino 86', 18, 'Irish', 4, '$$');
+VALUES (003, 'Green Irish', 'Via Dublino 86', 18, 'Irish', '***', '$$');
 
 INSERT INTO RED_RESTAURANTS
-VALUES (004, 'Japanese sushi', 'Via Tokyo 36', 40, 'Japanese', 3, '$$$');
+VALUES (004, 'Japanese sushi', 'Via Tokyo 36', 40, 'Japanese', '****', '$$$');
+
+DROP TABLE RED_USERS;
+
+DROP SEQUENCE USERS_SEQ;
+
+CREATE SEQUENCE USERS_SEQ;
+
+CREATE TABLE RED_USERS
+(USER_ID INTEGER PRIMARY KEY,
+FIRST_NAME VARCHAR2(30),
+LAST_NAME VARCHAR2(30),
+RELIABILITY VARCHAR2(5),
+REVIEWS INTEGER);
+
+INSERT INTO RED_USERS 
+VALUES (001, 'Giuseppe', 'Adamo', '**', 1);
+
+INSERT INTO RED_USERS 
+VALUES (002, 'Lorenzo', 'Barsocchi', '***', 3);
+
+INSERT INTO RED_USERS 
+VALUES (003, 'Stefano', 'Farati', '***', 5);
+
+INSERT INTO RED_USERS 
+VALUES (004, 'Charles', 'Dorian', '****', 7);
+
+
 
 
 
