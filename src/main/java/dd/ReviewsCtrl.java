578a11e45f314d83b28f408d7f6c41bd72066197
Lorenzo Barsocchi <barsocchi.lorenzo@gmail.com>
2019-06-05 12:06:05 +0200

    Attivazione Controller per Reviews

--- src/main/java/dd/ReviewsCtrl.java
+++ src/main/java/dd/ReviewsCtrl.java
@@ -3,11 +3,13 @@ package dd;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class ReviewsCtrl {
 	
 	@Autowired
